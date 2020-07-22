f9718a05c0cd17891f8aa802c0dbdee3b9226385
Bhavik Soni <bhaviksoni289@gmail.com>
2020-07-23 01:16:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URI =
-  'mongodb+srv://Bhavik:bscmongodb@cluster0.ld2qz.mongodb.net/shop';
+  '';
 
 const app = express();
 const store = new MongoDBStore({
